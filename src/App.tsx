7f96c15a999bc4b7a09581bf8345ad13001566ea
octanym <cmrnmr@gmail.com>
2022-07-16 23:16:40 -0400

    minor styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import './index.css';
+import React from 'react'
+import './index.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='bg-slate-600 min-h-screen flex items-center justify-center'>
+			<header className='bg-zinc-300 rounded-lg'>
+				<p className='px-4 py-2'>
+					Edit <code className='px-2 bg-black text-white hover:bg-blue-700 rounded-md'>src/App.tsx</code> and save
+					to reload.
+				</p>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
